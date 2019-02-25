a41096d08db0cfd218fa423023305ae794bc004d
Krzysztof Czepiec <kontakt@krzysztofczepiec.pl>
2019-02-25 21:19:34 +0100

    updateDiceImages

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -27,10 +27,18 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateDiceImages()
+        
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        updateDiceImages()
+        
+    }
+    
+    func updateDiceImages() {
         
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
@@ -39,6 +47,5 @@ class ViewController: UIViewController {
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
         
     }
-    
 }
 
